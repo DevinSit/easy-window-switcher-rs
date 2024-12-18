7ef7a0805afda72f65cf4e6874fa63caaed088c4
Devin Sit <devin@devinsit.com>
2024-12-18 11:41:45 -0500

    Removed Windows from Monitor model for now since we don't need it.

--- src/models/monitor.rs
+++ src/models/monitor.rs
@@ -1,18 +1,11 @@
-use super::Window;
-
 #[derive(Clone, Debug)]
 pub struct Monitor {
     pub width: i32,
     pub height: i32,
-    pub windows: Vec<Window>,
 }
 
 impl Monitor {
     pub const fn new(width: i32, height: i32) -> Self {
-        Self {
-            width,
-            height,
-            windows: vec![],
-        }
+        Self { width, height }
     }
 }
