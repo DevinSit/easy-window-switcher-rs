9d926aee73494d46b3522fd9e84348df91ba545d
Devin Sit <devin@devinsit.com>
2024-12-18 13:46:34 -0500

    Small refactoring in get_closest_window to shortcircuit earlier and do better error handling.

--- src/services/window_focuser.rs
+++ src/services/window_focuser.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{Ok, Result};
 use std::collections::HashMap;
 
 use crate::external_tools::{wmctrl, xdotool, xrandr};
@@ -83,45 +83,51 @@ fn get_closest_window(
     windows: &Vec<Window>,
     direction: &FocusDirection,
 ) -> Result<Option<Window>> {
+    if windows.is_empty() {
+        return Ok(None);
+    }
+
     let windows_by_monitor = index_windows_by_monitor(monitor_grid, windows)?;
     let monitors_by_window = index_monitors_by_window(monitor_grid, windows)?;
 
     let current_monitor = get_current_monitor(monitors_by_window);
     let current_monitor_windows = &windows_by_monitor[&current_monitor];
 
-    let current_window_position = current_monitor_windows
+    if let Some(current_window_position) = current_monitor_windows
         .iter()
         .position(|w| w.id == xdotool::get_current_focused_window_id())
-        .unwrap();
+    {
+        if is_closest_window_not_on_current_monitor(
+            direction,
+            current_monitor_windows,
+            current_window_position,
+        ) {
+            let mut next_monitor = monitor_grid.get_next_monitor(current_monitor, direction);
 
-    if windows.is_empty() {
-        Ok(None)
-    } else if is_closest_window_not_on_current_monitor(
-        direction,
-        current_monitor_windows,
-        current_window_position,
-    ) {
-        let mut next_monitor = monitor_grid.get_next_monitor(current_monitor, direction);
+            let mut optional_window =
+                get_window_from_monitor(&windows_by_monitor, next_monitor, direction);
 
-        let mut optional_window =
-            get_window_from_monitor(&windows_by_monitor, next_monitor, direction);
+            loop {
+                match optional_window {
+                    Some(window) => {
+                        return Ok(Some(window.clone()));
+                    }
+                    None => {
+                        next_monitor = monitor_grid.get_next_monitor(next_monitor, direction);
 
-        loop {
-            match optional_window {
-                Some(window) => {
-                    return Ok(Some(window.clone()));
-                }
-                None => {
-                    next_monitor = monitor_grid.get_next_monitor(next_monitor, direction);
-
-                    optional_window =
-                        get_window_from_monitor(&windows_by_monitor, next_monitor, direction);
+                        optional_window =
+                            get_window_from_monitor(&windows_by_monitor, next_monitor, direction);
+                    }
                 }
             }
+        } else {
+            let position = (current_window_position as i32 + direction.to_int()) as usize;
+            Ok(Some(current_monitor_windows[position].clone()))
         }
     } else {
-        let position: i32 = current_window_position as i32 + direction.to_int();
-        Ok(Some(current_monitor_windows[position as usize].clone()))
+        Err(anyhow::anyhow!(
+            "Invariant violated: current focused window not found on current monitor"
+        ))
     }
 }
 
